18b64329c711da2ef2214e402a85019cb2940f2e
Mr Zero <nguyenhoangtai2k@gmail.com>
2025-01-21 21:37:03 +0700

    Add project files.

---
+++ DesignPattern/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DesignPattern.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
