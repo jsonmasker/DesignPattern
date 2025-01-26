50b36a5c9fbbd46c1fb3628153ff980f98ba39cc
Mr Zero <nguyenhoangtai2k@gmail.com>
2025-01-26 20:02:17 +0700

    singleton pattern

--- SingletonPattern/Program.cs
+++ SingletonPattern/Program.cs
@@ -2,9 +2,21 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Task task1 = Task.Run(() =>
+            {
+                SingletonDesignPattern.Instance.Print(ConsoleColor.Red, 5);
+            });
+            Task task2 = Task.Run(() => {
+                SingletonDesignPattern.Instance.Print(ConsoleColor.Green, 5);
+            });
+            Task task3 = Task.Run(() =>
+            {
+                SingletonDesignPattern.Instance.Print(ConsoleColor.Blue, 5);
+            });
+            await Task.WhenAll(task1, task2, task3);
+            Console.ReadKey();
         }
     }
 }
---
+++ SingletonPattern/SingletonDesignPattern.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+    public sealed class SingletonDesignPattern
+    {
+        private static SingletonDesignPattern instance = null;
+        private static readonly object padlock = new object();
+        private SingletonDesignPattern()
+        {
+        }
+        public static SingletonDesignPattern Instance
+        {
+            get
+            {
+                lock (padlock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new SingletonDesignPattern();
+                    }
+                    return instance;
+                }
+            }
+        }
+        public void Print(ConsoleColor color, int x)
+        {
+            for (int i = 0; i < x; i++)
+            {
+                Console.ForegroundColor = color;
+                Console.WriteLine("Hello world");
+            }
+        }
+    }
+}
